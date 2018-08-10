59c001774cd8dc6310b7f0644b36dbb026613cf4
patrickfinnigan <patrickfinnigan94@gmail.com>
2018-08-09 21:25:48 -0700

    problem with the math logic in the homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,11 +31,31 @@ function intro() {
         type: "input",
         message: `Welcome to bamazon! What would you like to buy?
 Please enter the ID number of the item you wish to buy.`,
-    }).then( function (answer) {
-        let query = "SELECT * from bamazondb.market WHERE id = " + answer.action ;
-        database.query(query, { product: answer.action }, function (err, res) {
+    }).then(answer => {
+        let query = "SELECT * FROM bamazondb.market";
+        // you dont have to specify the exact row or column of the data for this function, since we define the exact data points we need later on down the code. the code above sets up all the data we need from mysql to the node program for us to draw from later. this means that we dont draw data directly from mysql, but rather take all the data into node and draw on specific parts of the data from there
+        database.query(query, {
+            id: answer.action
+        }, function (err, res) {
             if (err) throw err;
-            console.log(res[action].product_name).parseInt();
+            // console.log(res);
+            inquirer.prompt({
+                name: "quantity",
+                type: "input",
+                message: console.log("You have chosen to buy " + res[parseInt(answer.action) - 1].product_name + ", which there are " + res[parseInt(answer.action) - 1].stock_quantity + " left. How many would you like to purchase?")
+                // since the given id number will come in as a string, you have to convert it to an integer in order for the console.log action to read it and match that number with the proper product. that is why we use parseInt() around the given answer.action response
+            }).then(purchase => {
+                database.query(query, {
+                        id: purchase.action
+                    }, function (err, res) {
+                        if (err) throw err;
+                        const quantityMath = res[parseInt(answer.action) - 1].stock_quantity - parseInt(answer.purchase);
+                        console.log(quantityMath);
+                    }
+                    // let purchaseUpdate = "UPDATE bamazondb.market SET stock_quantity = stock_quantity WHERE address = " + 
+                )
+                process.exit()
+            })
         })
     });
 }
